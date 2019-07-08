78433e071ea2812b68fe67326a0246dfbe46b2bf
Luca <lucatannler@hotmail.com>
2019-07-08 13:12:45 +1000

    Ja
    ja

---
+++ 19T2-GAD173-Brief2-Taennler-1015901/19T2-GAD173-Brief2-Taennler-1015901/TileType.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "TileType.h"
+
+std::map<char, TileType*> TileType::tileTypes;
+std::map<char, sf::Texture*> TileType::tileTypeTextures;
+
+TileType::TileType(char pTileId, std::string pName, std::string tileSprite) : tileId(pTileId), name(pName), spriteLoc(tileSprite) {
+	
+}
+
+TileType::TileType() : tileId(' '), name(""), spriteLoc("") {
+
+}
+
+
+TileType::~TileType()
+{
+}
---
+++ 19T2-GAD173-Brief2-Taennler-1015901/19T2-GAD173-Brief2-Taennler-1015901/TileType.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef string
+#include <string>
+#endif // !string
+#ifndef map
+#include <map>
+#endif // !map
+#ifndef SFML/Graphics.hpp
+#include <SFML/Graphics.hpp>
+#endif // !SFML/Graphics.hpp
+#ifndef TileType.h
+#include "TileType.h"
+#endif // !TileType.h
+
+
+class TileType {
+public:
+	static std::map<char, TileType*> tileTypes;
+	static std::map<char, sf::Texture*> tileTypeTextures;
+
+	const char tileId;
+	const std::string name;
+	const std::string spriteLoc;
+
+	TileType(char pTileId, std::string pName, std::string tileSprite);
+	TileType();
+	~TileType();
+};
